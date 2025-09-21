4e4a20d94a7a9c938c19c7e43d715cf9fcc8a759
Nikita <hao.cheonma@gmail.com>
2025-09-21 22:06:01 +1000

    Update converterjson.h

--- src/converterjson.h
+++ src/converterjson.h
@@ -13,37 +13,37 @@ public:
 	ConverterJSON() = default;
 
 	/**
-	* Ìåòîä ïîëó÷åíèÿ ñîäåðæèìîãî ôàéëîâ
-	* @return Âîçâðàùàåò ñïèñîê ñ ñîäåðæèìûì ôàéëîâ ïåðå÷èñëåííûõ
-	* â config.json
+	* Метод получения содержащегося файла
+	* @return Возвращает список с содержащими файлами перечисленных
+	* в config.json
 	*/
 	static std::vector<std::string> getTextDocuments(); 
 	/**
-	* Ìåòîä ñ÷èòûâàåò ïîëå max_responses äëÿ îïðåäåëåíèÿ ïðåäåëüíîãî
-	* êîëè÷åñòâà îòâåòîâ íà îäèí çàïðîñ
+	* Метод считает поле max_responses для определения
+	* предельного количества ответов на один запрос
 	* @return
 	*/
 	static int getResponsesLimit();
 	/**
-	* Ìåòîä ïîëó÷åíèÿ çàïðîñîâ èç ôàéëà requests.json
-	* @return âîçâðàùàåò ñïèñîê çàïðîñîâ èç ôàéëà requests.json
+	* Метод получения запросов из файла requests.json
+	* @return возвращает список запросов из файла requests.json
 	*/
 	static std::vector<std::string> getRequests();
 	/**
-	* Ïîëîæèòü â ôàéë answers.json ðåçóëüòàòû ïîèñêîâûõ çàïðîñîâ
+	* Положить в файл answers.json результаты поисковых запросов
 	*/
 	static void putAnswers(std::vector<std::vector<std::pair<int, float>>> answers);
 	/**
-	* Ìåòîä ïîëó÷åíèÿ òåêñòà èç JSON ôàéëîâ
-	* @param path ïðèíèìàåò ïóòü ê ôàéëó
-	* @return âîçâðàùàåò èíôîðìàöèþ ñîäåðæàùóþñÿ â JSON ôàéëå
+	* Метод получения текста из JSON файлов
+	* @param path принимает путь к файлу
+	* @return возвращает информацию содержащуюся в JSON файле
 	*/
 	static nlohmann::json getJSONText(const std::string& path);
-	static const std::string configPath; // ïóòü ê êîíôèã ôàéëó
+	static const std::string configPath; // путь к конфиг файлу
 
 	static const std::string getName();
 	static const std::string getVersion();
 
 private:
-	static const std::string requestsPath; // ïóòü ê ôàéëó çàïðîñîâ
+	static const std::string requestsPath; // путь к файлу запросов
 };
