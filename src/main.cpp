db57dfaa8d2ef2d8b59ecb5ebeb496c65d52cb28
Nikita <hao.cheonma@gmail.com>
2025-09-21 21:59:15 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
 #include "searchengine.h"
+#include "converterjson.h"
 
-int main() { SearchEngine::startSearch(); }
+int main() { 
+  SearchEngine::startSearch(); 
+  std::cout << converter.getName() << "[" << converter.getVersion() << "] finish job\n";
+}
